28599543a12e19d439121ec743617417fca3c8a2
David Heisnam <davidheisnam@gmail.com>
2019-06-25 15:55:54 +0530

    feat: Add autokill after timeout

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import socketio
 from flask import Flask, render_template, url_for
 import os
+import signal
 import sys
 import subprocess
 
@@ -11,11 +12,65 @@ import fcntl
 import _thread as thread
 import time
 
+
 sio = socketio.Server(async_mode='threading')
 app = Flask(__name__)
 app.wsgi_app = socketio.Middleware(sio, app.wsgi_app)
 
 execute_python_version = 2
+
+def kill_process(pid):
+    try:
+        os.kill(pid, signal.SIGTERM) #or signal.SIGKILL
+    except:
+        pass
+
+
+def add_process_start_time_to_session(sid, pid):
+    session = sio.get_session(sid)
+    if session:
+        session['process_start_time'] = pid
+    else:
+        session = {'process_start_time': pid}
+    sio.save_session(sid, session)
+
+
+def add_pid_to_session(sid, pid):
+    # Save the pid of the child process spawned
+    session = sio.get_session(sid)
+    if session:
+        session['running_process_id'] = pid
+    else:
+        session = {'running_process_id': pid}
+    sio.save_session(sid, session)
+    return session
+
+
+def add_user_input_to_session(sid, input):
+    session = sio.get_session(sid)
+    if session:
+        session['user_input_line'] = input
+    else:
+        session = {'user_input_line': input}
+    sio.save_session(sid, session)
+    return session
+
+
+def get_running_process_id(sid):
+    session = sio.get_session(sid)
+    if session and 'running_process_id' in session:
+        return session['running_process_id']
+    else:
+        return None
+
+
+def get_user_input_line(sid):
+    session = sio.get_session(sid)
+    if session and 'user_input_line' in session:
+        return session['user_input_line']
+    else:
+        return None
+
 
 @app.route('/')
 def get_index():
@@ -36,31 +91,33 @@ def disconnect(sid):
 
 @sio.on('run_program')
 def run_program(sid, data):
-    sio.save_session(sid, {'user_input_line': None})
     proc = None
     if execute_python_version == 3:
         proc = subprocess.Popen(['python3', 'run_program.py', data], stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=0)
     else:
         proc = subprocess.Popen(['python2.7', '-u', 'run_program_2.py', data], stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1)
+    
     set_non_block(proc.stdout)
-    thread.start_new_thread(prompt_user, (sid, proc, ))
+
+    running_process_id = get_running_process_id(sid)
+    if running_process_id:
+        print('KILLING PROCESS')
+        kill_process(running_process_id)
+    
+    sio.save_session(sid, {'user_input_line': None, 'running_process_id': proc.pid})
+    thread.start_new_thread(start_io_loop, (sid, proc, ))
 
 
 @sio.on('user_input')
 def user_input(sid, data):
-    sio.save_session(sid, {'user_input_line': data})
+    add_user_input_to_session(sid, data)
 
 
-def prompt_user(sid, proc):
-    while True:
-        if proc.poll() is not None:
-            # print('Process already stopped')
-            sio.emit('stdout', {'data': 'Program finished'})
-            sys.stdout.flush()
-            break
-        session = sio.get_session(sid)
-        user_input_line = session['user_input_line']
-        sio.save_session(sid, {'user_input_line': None})
+def start_io_loop(sid, proc):
+    while True:        
+        user_input_line = get_user_input_line(sid)
+        add_user_input_to_session(sid, None)
+
         if user_input_line:
             proc.stdin.write('{}{}'.format(user_input_line,'\n').encode('utf-8'))
             proc.stdin.flush()
@@ -68,6 +125,11 @@ def prompt_user(sid, proc):
         if line:
             sio.emit('stdout', {'data': line.decode('utf-8')})
             time.sleep(0.2)
+
+        if proc.poll() is not None:
+            sio.emit('stdout', {'data': 'program_finished'})
+            add_pid_to_session(sid, None)
+            break
 
 
 '''
--- run_program.py
+++ run_program.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
+# Run Python 3 scripts passed as argument
+
+import os
+import signal
+import sys
 import argparse
 import base64
-import sys
+import time
+import _thread as thread
+
+program_start_time = 0
+program_running = True
 
 def get_script_from_args():
     parser = argparse.ArgumentParser(description='Get script')
@@ -10,8 +19,23 @@ def get_script_from_args():
     args = parser.parse_args()
     return args.script
 
+def start_autostop_loop():
+    while True:
+        if not program_running:
+            break
+        check_and_stop_program()
+        time.sleep(1)
+
+def check_and_stop_program():
+    elapsed_time = time.time() - program_start_time
+    if elapsed_time > 300: # Stop process after 5 minutes
+        os.kill(os.getpid(), signal.SIGTERM)
+
 
 if __name__ == '__main__':
+    program_start_time = time.time()
+    thread = thread.start_new_thread(start_autostop_loop, ())
+
     encoded_script_string = get_script_from_args()
     decoded_code_string = base64.b64decode(encoded_script_string)
     try:
@@ -20,3 +44,4 @@ if __name__ == '__main__':
         print(e)
         sys.stdout.flush()
         pass
+    program_running = False
